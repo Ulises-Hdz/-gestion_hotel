e986b30c0141fac9375b631c6c916aef38a51f30
Ulises Hernandez <Dogzack12@gmail.com>
2024-11-11 20:39:15 -0600

    Se agregaron clases al hotel (habitacion, reserva , cliente).

--- Hotel.py
+++ Hotel.py
@@ -2,4 +2,13 @@ class Habitacion:
     def  __init__ (self , tipo , numero):
         self.numero = numero 
         self.tipo = tipo 
-        
+class Cliente:
+    def __init__ (self , nombre , dni ):
+        self.nombre = nombre 
+        self.dni = dni 
+class Reserva:
+    def __init__ (self , dias , habitacion , cliente ):
+        self.dias = dias 
+        self.habitacion = habitacion 
+        self.cliente = cliente   
+                 
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # -gestion_hotel 
-Esto va gestionar un hotel 
+Esto va gestionar un hotel. Se van a crear las clases de cada objeto del hotel incluso las reservas, clientes, habitaciones etc.
