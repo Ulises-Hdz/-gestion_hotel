b7f0dece314c5665ecfb61f500871fe1502a4245
Ulises Hernandez <Dogzack12@gmail.com>
2024-11-13 21:55:56 -0600

    Se agregaron algunas funciones del hotel.

--- Hotel.py
+++ Hotel.py
@@ -15,6 +15,24 @@ class ServicioCuarto:
     def __init__ (self , habitacion , cliente ):
         self.habitacion = habitacion 
         self.cliente = cliente 
-def Agregar_habitacion (self):
-    numero = int(input("Ingrese el numero de la habitacion:"))
-    tipo = input("Elige que tipo de habitacion quiere (Sencilla/Doble)")
+class Hotel:
+    def __init__ (self ,nombre ):
+        self.nombre = nombre 
+        self.habitaciones = []
+        self.clientes= []
+        self.reservas = []
+    def agregar_habitacion (self):
+        numero = int(input("Ingrese el numero de la habitacion:"))
+        tipo = input("Elige que tipo de habitacion quiere (Junior Suite/Suite/Master Suite):")
+        habitacion = Habitacion(numero , tipo)
+        self.habitaciones.append(habitacion)
+        print(f"Habitación {numero} de tipo {tipo} agregada correctamente.")
+        self.habitaciones.append(habitacion)
+    def registrar_cliente (self):
+        nombre = input("Ingrese el nombre del cliente:")
+        dni = input("Ingrese el DNI del cliente:")
+        cliente = Cliente(nombre , dni )
+        self.clientes.append(cliente)
+        print(f"El cliente {cliente} se registro correctamente.")
+
+ 
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # -gestion_hotel 
-Esto va gestionar un hotel. Se van a crear las clases de cada objeto del hotel incluso las reservas, clientes, habitaciones etc. Se crean los metodos de cada clases.
+Esto va gestionar un hotel. Se van a crear las clases de cada objeto del hotel incluso las reservas, clientes, habitaciones etc. Se crean los metodos de cada clases. Se crean las funciones que va tener el hotel.
